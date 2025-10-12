13356204b2f7a083a0476fb9e5a1551b07b7ae9f
Minjie Zha <minjiezha@gmail.com>
2025-10-11 22:02:10 -0700

    delete ellips

--- src/tasks/task.py
+++ src/tasks/task.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Field
 
 
 class Task(BaseModel):
-    title: str = Field(..., description="The title of the task.")
+    title: str = Field(description="The title of the task.")
